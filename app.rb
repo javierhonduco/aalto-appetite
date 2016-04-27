54dd5767b58d938d1d96e239c6653af3d3458422
Javier <javierhonduco@gmail.com>
2016-04-27 12:37:14 +0300

    First version

---
+++ app.rb
@@ -0,0 +1,54 @@
+require 'httparty'
+require 'nokogiri'
+
+ENDPOINTS = {
+  :taffa => 'https://www.teknologforeningen.fi/?lang=en',
+  :alvari => 'http://www.amica.fi/modules/json/json/Index?costNumber=0190&language=en',
+  :tuas => 'http://www.amica.fi/modules/json/json/Index?costNumber=0199&language=en',
+  :cs => 'http://www.sodexo.fi/ruokalistat/output/daily_json/142/2016/04/26/fi',
+  :valimo => 'http://www.sodexo.fi/ruokalistat/output/daily_json/13918/2016/04/26/fi'
+}
+
+STRATEGIES = {
+  :cs => :sodexo_json,
+  :valimo => :sodexo_json,
+  :tuas => :amica_parsing,
+  :alvari => :amica_parsing,
+  :taffa => :taffa_parsing
+}
+
+def parse_taffa url
+  response = HTTParty.get(url)
+  parsed = Nokogiri.parse(response)
+  parsed.css('.todays-menu > ul > li').map {|dish|
+    dish.text
+  }
+end
+
+def parse_sodexo url
+  response = HTTParty.get(url)
+  response['courses'].map { |course|
+    course['title_en']
+  }
+end
+
+def parse_amica url
+  response = HTTParty.get(url)
+  response['MenusForDays'].first['SetMenus'].map { |menu|
+    menu['Components']
+  }
+end
+
+def get_menu cafeteria
+  endpoint = ENDPOINTS[cafeteria]
+  result = case STRATEGIES[cafeteria]
+    when :sodexo_json
+      parse_sodexo(endpoint)
+    when :amica_parsing
+      parse_amica(endpoint)
+    when :taffa_parsing
+      parse_taffa(endpoint)
+  end
+
+  result
+end
